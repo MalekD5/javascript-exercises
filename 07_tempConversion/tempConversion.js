6fed5cfda495f77752f5e81c4b1105d51af58ef1
Invvk <106095300+MalekD5@users.noreply.github.com>
2022-08-27 15:44:01 -0500

    solve temp conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fren) {
+  const result = (fren - 32) * (5/9);
+  return parseFloat(result.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  const result = cel * (9/5) + 32;
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
