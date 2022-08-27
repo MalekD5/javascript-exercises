5c200e444edba8b66a9bb47295bfc51f4e0edd7c
Invvk <106095300+MalekD5@users.noreply.github.com>
2022-08-27 14:53:59 -0500

    solve repeat exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeat) {
+      if (repeat < 0)
+            return 'ERROR';
+      let repeated = '';
+      for (let i = 0; i < repeat; i++)
+            repeated += text;
+      return repeated;
 };
 
 // Do not edit below this line
