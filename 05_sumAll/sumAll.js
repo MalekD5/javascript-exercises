923c8f98e6f1b792ac4ccb36b44a8a8797655578
Invvk <106095300+MalekD5@users.noreply.github.com>
2022-08-27 15:29:12 -0500

    solve sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+      if (typeof start !== 'number' || typeof end !== 'number')
+            return 'ERROR';
+      if (start < 0 || end < 0) 
+            return 'ERROR'
+      
+      if (end < start) {
+            const temp = start;
+            start = end;
+            end = temp;
+      }
 
+      if (start === end)
+            return end;
+      return start + sumAll(start+1, end);
 };
 
 // Do not edit below this line
