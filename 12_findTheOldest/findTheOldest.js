08f4f31859b98f032db40c0c649702bf3dfd0384
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2022-09-15 04:42:40 -0700

    solve find the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(object) {
+    const currentYear = new Date().getFullYear();
 
+    return object.reduce((a, b) => {
+        if (Object.keys(a).length == 0) { return b; }
+        
+        const first = ((a.yearOfDeath) ? a.yearOfDeath : currentYear) - a.yearOfBirth;
+        const second = ((b.yearOfDeath) ? b.yearOfDeath : currentYear) - b.yearOfBirth;
+        const aa = first < second ? b : a;
+        return aa;
+    }, {});
 };
 
 // Do not edit below this line
